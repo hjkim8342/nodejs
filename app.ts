e0f016596f0a2e7e55983457f3446affc6fa307e
hjkim8342 <hjkim8342@naver.com>
2024-01-22 12:25:57 +0900

    456

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import * as http from "http";
+import fs from "fs";
 
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
     const url = req.url;
+    const method = req.method;
     if (url === "/") {
       res.write("<html>");
       res.write(`<head><title>${"Enter Message"}</title></head>`);
@@ -16,6 +18,23 @@ const server = http.createServer(
       `);
       res.write("</html>");
       return res.end();
+    }
+    if (url === "/message" && method === "POST") {
+      const body: Buffer[] = [];
+      req.on("data", (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+      });
+      req.on("end", () => {
+        const parseBody = Buffer.concat(body).toString();
+        console.log(parseBody);
+        const message = parseBody.split("=")[1];
+        fs.writeFile("message.txt", message, (err) => {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          return res.end();
+        });
+      });
     }
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
