2456145a7ae68df3a2e758372124a7f5fff65bca
hjkim8342 <hjkim8342@naver.com>
2024-01-22 18:54:54 +0900

    20240122 챕터3 마무리

--- app.ts
+++ app.ts
@@ -1,48 +1,6 @@
-import * as http from "http";
-import fs from "fs";
+import http from "http";
+import routes from "./routes";
 
-const server = http.createServer(
-  (req: http.IncomingMessage, res: http.ServerResponse) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === "/") {
-      res.write("<html>");
-      res.write(`<head><title>${"Enter Message"}</title></head>`);
-      res.write(`
-      <body>
-        <form action="/message" method="POST">
-          <input type="text" name="message" />
-          <button type="submit">Send</button>
-        </form>
-      </body>
-      `);
-      res.write("</html>");
-      return res.end();
-    }
-    if (url === "/message" && method === "POST") {
-      const body: Buffer[] = [];
-      req.on("data", (chunk) => {
-        console.log(chunk);
-        body.push(chunk);
-      });
-      req.on("end", () => {
-        const parseBody = Buffer.concat(body).toString();
-        console.log(parseBody);
-        const message = parseBody.split("=")[1];
-        fs.writeFile("message.txt", message, (err) => {
-          res.statusCode = 302;
-          res.setHeader("Location", "/");
-          return res.end();
-        });
-      });
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-    res.write("</html>");
-    res.end();
-  }
-);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
