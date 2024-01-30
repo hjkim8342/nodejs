a958cb3fea075b276ad7264159073bc6e4871057
hjkim8342 <hjkim8342@naver.com>
2024-01-30 17:42:54 +0900

    pub > handlebars

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.engine("handlebars", expressHbs());
+app.set("view engine", "handlebars");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
